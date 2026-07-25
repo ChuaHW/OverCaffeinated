63f2865d199effbc86e62af536940d0bfa47ba10
Han Wei <E1525291@u.nus.edu>
2026-07-25 09:42:32 +0800

    update db config

--- server/db.js
+++ server/db.js
@@ -2,10 +2,12 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
 });
 
 module.exports = pool.promise();
